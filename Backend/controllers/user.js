69144bed15a8132a928522d01f146cd394013aa5
Abbie <abbie@github.com>
2019-11-08 00:43:59 +0100

    Added JWT

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,12 +1,14 @@
 const { pool } = require('../pgconnect');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 exports.signup = (request, response, next) => {
   bcrypt.hash(request.body.password, 10)
     .then((hash) => {
       const values = [request.body.username, request.body.firstname, request.body.lastname, request.body.email, hash, request.body.genderid, request.body.departmentid, request.body.jobrole_id, request.body.address]
-      pool.query( `INSERT INTO users (username, firstname, lastname, email, password, genderid, departmentid, roleid, address, last_login)
+      
+      pool.query( `INSERT INTO users (username, firstname, lastname, email, password, genderid, departmentid, roleid, address, created_at)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, now())`, [...values], (error, results) => {
         if(error) {
           response.status(400).json({
@@ -18,7 +20,6 @@ exports.signup = (request, response, next) => {
             message: 'User created successfully!'
           }
           );
-          console.log(JSON.stringify(results));
         }
       });
     }).catch((error) => {
@@ -29,7 +30,7 @@ exports.signup = (request, response, next) => {
 exports.login = (request, response, next) => {
     try {
     const values = request.body.email;
-    pool.query("SELECT u.email, u.password FROM users u WHERE u.email = $1 LIMIT 1", [values], (error, results) => {
+    pool.query("SELECT u.userid, u.email, u.password FROM users u WHERE u.email = $1 LIMIT 1", [values], (error, results) => {
         if(results.rows.length === 0){
                 return response.json({
                     error: 'Invalid email'
@@ -43,10 +44,19 @@ exports.login = (request, response, next) => {
                     error: 'Invalid password'
                 });
             }
-                return response.send({
-                    success: true,
-                    message: 'Log in Successful!'
-                });
+            });
+
+            const token = jwt.sign(
+                { userId: request.body.userid },
+                process.env.TOKEN,
+                { expiresIn: '24h'}
+            );
+
+            return response.send({
+                status: 'Success!',
+                message: 'Log in Successful!',
+                token: token,
+                userid: results.rows[0].userid
             });
         });
     }catch (error) {
