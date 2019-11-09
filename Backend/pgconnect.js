5520a7be7cfa13d4d57f1ad601d1ea2084f6d383
Abbie <abbie@github.com>
2019-11-09 12:14:00 +0100

    Added new dependencies in package.json

--- Backend/pgconnect.js
+++ Backend/pgconnect.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-import { pool } from 'pg';
+const { Pool } = require('pg');
 const productionVar = process.env.NODE_ENV === 'production'
 
 const connection = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
