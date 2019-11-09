3dd15b1912c9bfdbad030be2314850827aaf3701
Abbie <abbie@github.com>
2019-11-09 23:11:26 +0100

    Update article route created

--- Backend/controllers/articles.js
+++ Backend/controllers/articles.js
@@ -11,11 +11,31 @@ exports.createArticle = (request, response, next) => {
                 response.json({ 
                     status: 'Success!',
                     Data: {
-                    message: 'Posted successfully!',
+                    message: 'Article successfully posted!',
                     createon: request.body.created_on,
                     title: request.body.title
                     }
                 });
             }
     });
+}
+
+exports.updateArticle = (request, response, next) => {
+    const id = parseInt(request.params.id);
+    const values = Object.values(request.body)
+    pool.query('UPDATE articles SET authorid=$1, title=$2, article=$3 WHERE articleid=$4',
+    [...values, id], (error, results) => {
+        if (error){
+            response.status(400).json({ error: 'Failed to update!' });
+        }else {
+            response.json({ 
+                status: 'Success',
+                Data: {
+                message: 'Article successfully updated!',
+                title: request.body.title,
+                article: request.body.article
+                }
+            });
+        }
+    })
 }
