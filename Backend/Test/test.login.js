73408dd459f0fcceafbf235e5b78e1fa2f166446
Abbie <abbie@github.com>
2019-11-09 20:38:48 +0100

    Test files

---
+++ Backend/Test/test.login.js
@@ -0,0 +1,23 @@
+const expect = require('chai').expect;
+const chai = require('chai');
+const chaihttp = require('chai-http');
+const baseUrl = 'http://localhost:5000/api/v1/auth';
+
+describe('user log in', () => {
+    it('logs in', (done) => {
+    const userDetails = {
+        // LOG IN TEST!
+        email: "Testie3@test.com",
+        password: "Testieequalstruthy"
+    };
+    chai.request(baseUrl)
+        .post('/login')
+        .send(
+           userDetails
+        )
+        .end((error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            done();
+         });
+});
+})
