8cb2935fc09d3d38efca305732540a457af3155f
Abbie <abbie@github.com>
2019-11-09 12:35:02 +0100

    required bodyparser and added app.use for user route

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const userRoutes = require('./routes/user');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api/v1/auth', userRoutes);
 
 module.exports = app;
