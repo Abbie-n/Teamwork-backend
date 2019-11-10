3459aafccf0cbb89adf0ff3666792def14a92ad9
Abbie <abbie@github.com>
2019-11-10 23:12:28 +0100

    added authentication to routes

--- Backend/Routes/articles.js
+++ Backend/Routes/articles.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {auth} = require('../Middleware/auth');
 
 const articleCtrl = require('../controllers/articles');
 
 // ARTICLES ROUTES
 
-router.post('/post', articleCtrl.createArticle);
-router.patch('/:id', articleCtrl.updateArticle);
-router.delete('/:id', articleCtrl.deleteOneArticle);
-router.post('/:id/comment', articleCtrl.createComment);
-router.get('/:id', articleCtrl.getOneArticle);
+router.post('/post', auth, articleCtrl.createArticle);
+router.patch('/:id', auth, articleCtrl.updateArticle);
+router.delete('/:id', auth, articleCtrl.deleteOneArticle);
+router.post('/:id/comment', auth, articleCtrl.createComment);
+router.get('/:id', auth, articleCtrl.getOneArticle);
 
 module.exports = router;
--- Backend/Routes/gifs.js
+++ Backend/Routes/gifs.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const {auth} = require('../Middleware/auth');
 
 const gifCtrl = require('../controllers/gifs');
 
 // GIFS ROUTES
-router.post('/upload', gifCtrl.createGif);
-router.delete('/:id', gifCtrl.deleteOneGif);
-router.post('/:id/comment', gifCtrl.createComment);
-router.get('/:id', gifCtrl.getOneGif);
+router.post('/upload', auth, gifCtrl.createGif);
+router.delete('/:id', auth, gifCtrl.deleteOneGif);
+router.post('/:id/comment', auth, gifCtrl.createComment);
+router.get('/:id', auth, gifCtrl.getOneGif);
 
 module.exports = router;
