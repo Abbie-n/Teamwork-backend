20a31abfd4d0cdbdae05f6ea418d75dc57b215c9
Abbie <abbie@github.com>
2019-11-21 07:26:03 +0100

    Fi...nal

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ const normalizePort = (val) => {
   }
   return false;
 };
-const PORT = normalizePort(process.env.PORT || '8000');
-app.set('port', PORT);
+const port = normalizePort(process.env.PORT || '8000');
+app.set('port', port);
 
 const errorHandler = (error) => {
   if (error.syscall !== 'listen') {
     throw error;
   }
   const address = server.address();
-  const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${PORT}`;
+  const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${port}`;
   switch (error.code) {
     case 'EACCES':
       console.error(`${bind} requires elevated privileges.`);
@@ -44,8 +44,8 @@ const server = http.createServer(app);
 server.on('error', errorHandler);
 server.on('listening', () => {
   const address = server.address();
-  const bind = typeof address === 'string' ? `pipe ${address}` : `port ${PORT}`;
+  const bind = typeof address === 'string' ? `pipe ${address}` : `port ${port}`;
   console.log(`Listening on ${bind}`);
 });
 
-server.listen(PORT);
+server.listen(port);
